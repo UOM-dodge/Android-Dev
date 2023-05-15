5d550e071fd1e70bf0a86bc6553cdeed3391afcd
Chrysanthitre <chrysanthitrentsiou@gmail.com>
2023-05-16 01:28:58 +0300

    Initial Commit

--- Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
@@ -3,12 +3,69 @@ package com.example.curemeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button timeButton;
+    int hour, minute;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timeButton = findViewById(R.id.buttonTime);
+
+        Spinner spinnerActions = (Spinner) findViewById(R.id.spinner_actions);
+
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.actions,
+                        android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinnerActions.setAdapter(adapter);
+
+
+
+    }
+
+    public void popTimePicker(View view)
+    {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener()
+        {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute)
+            {
+                hour = selectedHour;
+                minute = selectedMinute;
+             timeButton.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+            }
+        };
+
+        //int style = AlertDialog.THEME_HOLO_DARK;
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+
+        timePickerDialog.setTitle("Ώρα επίσκεψης");
+        timePickerDialog.show();
+
     }
 }
