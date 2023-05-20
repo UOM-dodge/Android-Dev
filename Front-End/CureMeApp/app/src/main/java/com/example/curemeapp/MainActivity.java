ce05cc389622138e505144f64ce1c30052b71b2f
UOM-dodge <ics21004@uom.edu.gr>
2023-05-20 16:41:56 +0300

    early commit

--- Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.curemeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    CalendarFragment calendarFragment = new CalendarFragment();
+    HistoryFragment historyFragment = new HistoryFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.home) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                    return true;
+                } else if (itemId == R.id.calendar) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, calendarFragment).commit();
+                    return true;
+                } else if (itemId == R.id.history) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, historyFragment).commit();
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
