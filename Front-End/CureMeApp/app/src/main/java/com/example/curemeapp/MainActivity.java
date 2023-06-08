368ab8aeed225803f0652543f4750aeb202fad39
ch.samaras <54262610+ch.samaras@users.noreply.github.com>
2023-06-08 18:32:36 +0300

    inital commit and functionality

--- Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.curemeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button r1Btn = findViewById(R.id.r1Btn);
+        r1Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, R1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+//        Button r2Btn = findViewById(R.id.r2Btn);
+//        r2Btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, R2Activity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
+
 }
---
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/R1Activity.java
@@ -0,0 +1,11 @@
+package com.example.curemeapp;
+
+import android.os.Bundle;
+
+public class R1Activity extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.r1);
+    }
+}
