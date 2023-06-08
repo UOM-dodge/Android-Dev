6926a8c489fb4d6aab35b767609a630008e97a42
Christiana <christigioti@gmail.com>
2023-06-08 22:01:11 +0300

    initial commit

---
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/CalendarViewHolder.java
@@ -0,0 +1,29 @@
+package com.example.curemeapp;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public final View parentView;
+    public final TextView dayofMonth;
+    private final CalendarAdapter.OnItemListener onItemListener;
+    public CalendarViewHolder( @NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
+    {
+        super(itemView);
+        parentView = itemView.findViewById(R.id.parentView);
+        dayofMonth = itemView.findViewById(R.id.cellDayText);
+        this.onItemListener = onItemListener;
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        onItemListener.onItemClick(getAdapterPosition(), (String) dayofMonth.getText());
+    }
+
+}
--- Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/WeekViewActivity.java
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/WeekViewActivity.java
@@ -1,9 +1,8 @@
 package com.example.curemeapp;
 
-import static com.example.curemeapp.CalendarUtils.daysInMonthArray;
+import static com.example.curemeapp.CalendarUtils.daysInWeekArray;
 import static com.example.curemeapp.CalendarUtils.monthYearFromDate;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -16,19 +15,18 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener{
+public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener{
 
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_week_view);
         initWidgets();
         CalendarUtils.selectedDate = LocalDate.now();
-        setMonthView();
+        setWeekView();
     }
 
     private void initWidgets()
@@ -37,46 +35,34 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
         monthYearText = findViewById(R.id.MonthYearTV);
     }
 
-    private void setMonthView()
+    private void setWeekView()
     {
         monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
-        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+        ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.selectedDate);
 
-        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
         calendarRecyclerView.setLayoutManager(layoutManager);
         calendarRecyclerView.setAdapter(calendarAdapter);
 
     }
 
-
-    public void previousMonthAction(View view)
+    public void previousWeekAction(View view)
     {
-        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
-        setMonthView();
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
+        setWeekView();
     }
 
-    public void nextMonthAction(View view)
+    public void nextWeekAction(View view)
     {
-        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
-        setMonthView();
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
+        setWeekView();
     }
 
-    @Override
     public void onItemClick(int position, String dayText)
     {
-       if(dayText.equals(""))
-       {
-           String message = "Selected Date " + dayText + " " + monthYearFromDate(CalendarUtils.selectedDate);
-           Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-       }
-
+            String message = "Selected Date " + dayText + " " + monthYearFromDate(CalendarUtils.selectedDate);
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
-
-    public void weeklyAction(View view)
-    {
-        startActivity(new Intent(this ,WeekViewActivity.class));
-    }
-
 
 }
