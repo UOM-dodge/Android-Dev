9adcfd9823745a6f31c246362b0b41fc76cda04f
Christiana <christigioti@gmail.com>
2023-06-08 22:58:14 +0300

    test commit

--- Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity implements CalendarAdapter.O
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidgets();
