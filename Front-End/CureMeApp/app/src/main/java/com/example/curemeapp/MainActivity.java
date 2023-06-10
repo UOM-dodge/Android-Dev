30feaf1756902997a27dc53c22bb4c14f5f7e1b0
UOM-dodge <ics21004@uom.edu.gr>
2023-06-10 16:34:26 +0300

    welcome screen

--- Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.curemeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        finish();
+        overridePendingTransition(5,5);
+        Intent intent = new Intent(this, LogInActivity.class);
+        startActivity(intent);
+        overridePendingTransition(5,5);
 
     }
 }
