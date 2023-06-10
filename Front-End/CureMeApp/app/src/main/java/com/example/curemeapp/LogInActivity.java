2ff38d58d6a5ac49c5beee52a947d015f84d445a
UOM-dodge <ics21004@uom.edu.gr>
2023-06-10 16:44:57 +0300

    R1 successful merge

--- Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/LogInActivity.java
+++ Front-End/CureMeApp/app/src/main/java/com/example/curemeapp/LogInActivity.java
@@ -1,6 +1,9 @@
 package com.example.curemeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +13,14 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
 
-
+        Button logInBtn = findViewById(R.id.r1Btn);
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivityR1.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
